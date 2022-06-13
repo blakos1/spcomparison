2d5de8c7e3b59b9c8d36715c1393de9a2fb1bae3
blakos1 <blakos1@st.amu.edu.pl>
2022-06-14 01:47:36 +0200

    inits pt2

---
+++ spcomparison_survey.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(shinyforms)
+library(googlesheets4)
+
+questions <- list(
+  list(id = "name", type = "text", title = "Name", mandatory = TRUE),
+  list(id = "age", type = "numeric", title = "Age"),
+  list(id = "favourite_pkg", type = "text", title = "Favourite R package"),
+  list(id = "terms", type = "checkbox", title = "I agree to the terms")
+)
+
+formInfo <- list(
+  id = "basicinfo",
+  questions = questions,
+  storage = list(
+    # Right now, only flat file storage is supported
+    type = STORAGE_TYPES$FLATFILE,
+    # The path where responses are stored
+    path = "responses"
+  )
+)
+
+ui <- fluidPage(
+  formUI(formInfo)
+)
+
+server <- function(input, output, session) {
+  formServer(formInfo)
+}
+
+shinyApp(ui = ui, server = server)
+
