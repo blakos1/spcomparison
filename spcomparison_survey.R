e2878bea6e7256e527d74879cfef92870b1039e4
blakos1 <blakos1@st.amu.edu.pl>
2022-06-28 22:13:31 +0200

    adds randomized questions with images

--- spcomparison_survey.R
+++ spcomparison_survey.R
@@ -4,54 +4,77 @@ library(googlesheets4)
 library(dplyr)
 library(tidyr)
 
-# questions ----
-df1 <- data.frame(question = '<p>tags$img(src = myImgResources[1], width = "200px", height = "200px")</p>',
-                 option = NA,
-                 input_type = "textSlider",
-                 input_id = "q1",
-                 dependence = NA,
-                 dependence_value = NA,
-                 required = T,
-                 page = 1)
+gsheets = "https://docs.google.com/spreadsheets/d/1oEMCCRoSdRS-iiXGgWlku2h-4SgOREkCv65z2BxRsRE/edit?usp=sharing"
+sheet_test = read_sheet(gsheets, col_names = FALSE)
 
-df2 = df1
-df3 = df1
 
-df2$question = "pytanie 2"
-df2$input_id = "q2"
-df2$page = 2
-
-df3$question = "pytanie 3"
-df3$input_id = "q3"
-df3$page = 3
-
-questions_df = rbind(df1,df2)
-questions_df = rbind(questions_df,df3)
-
-# images ----
-myImgResources <- paste0("imgResources/img", seq_len(2), ".jpg")
+# images ------------------------------------------------------------------
+myImgResources <- paste0("imgResources/img", seq_len(10), ".jpg")
 
 # Add directory of static resources to Shiny's web server
 addResourcePath(prefix = "imgResources", directoryPath = "resources")
 
 
-# app ----
+# questions ---------------------------------------------------------------
+dataset = combn(myImgResources, 2)
+dataset = as.data.frame(dataset)
+dataset = as.list(dataset)
+
+datasample = sample(dataset, 10)
+# x[[1]][1]
+
+
+questions_df = tibble::tibble()
+
+for (i in 1:length(datasample)){
+  question_id = datasample[[i]] %>% 
+    stringr::str_remove_all("imgResources/") %>%
+    stringr::str_remove_all(".jpg") %>%
+    paste(collapse = '+')
+  
+  question = tibble::tibble(question = list(div(img(src=datasample[[i]][1], width="49%", height="250vh"),
+                                                img(src=datasample[[i]][2], width="49%", height="250vh"),
+                                                br(),
+                                                paste0("Pytanie ", i, ": Określ podobieństwo między dwoma obrazami"))),
+                            option = NA,
+                            input_type = "textSlider",
+                            input_id = question_id,
+                            dependence = NA,
+                            dependence_value = NA,
+                            required = T,
+                            page = i)
+  
+  
+  questions_df = rbind(questions_df, question)
+}
+
+
+# app ---------------------------------------------------------------------
+sliderScale <<- c(
+  "Brak",
+  "Bardzo male",
+  "Niewielkie ",
+  "Trudno powiedziec",
+  "Lekkie",
+  "Duze",
+  "Calkowite"
+) #global variable bo inaczej nie dziala w linijce 63
+
+
 extendInputType(input_type = "textSlider", {
   shinyWidgets::sliderTextInput(
     inputId = surveyID(),
     label = surveyLabel(),
     force_edges = TRUE,
-    choices = c(LETTERS[1:7]),
-    selected = LETTERS[4],
+    choices = sliderScale,
+    selected = sliderScale[4],
     grid = TRUE
   ) 
 })
 
 ui <- fluidPage(
-  tags$img(src = myImgResources[1], width = "250px", height = "250px"),
-  tags$img(src = myImgResources[2], width = "250px", height = "250px"),
   surveyOutput(df = questions_df,
-               survey_title = "Porównanie metod określania zmian struktury przestrzennej kategorii pokrycia terenu",
+               survey_title = "Zmiany struktury przestrzennej kategorii pokrycia terenu",
                survey_description = "Opis ankiety?")
 )
 
@@ -59,12 +82,20 @@ server <- function(input, output, session) {
   renderSurvey()
   
   observeEvent(input$submit, {
+    response_data = getSurveyData()
+    response_data = subset(response_data, select = -c(question_type))
+    print(response_data)
+    sheet_append(gsheets, data = response_data)
+    
     showModal(modalDialog(
       title = "Dziękuję za wypełnienie ankiety",
-      "Kontakt: blakos1@st.amu.edu.pl"
+      "Kontakt: blakos1@st.amu.edu.pl",
+      footer = actionButton("dismiss", "Powrót do strony głównej")
       #"You can customize what actions happen when a user finishes a survey using input$submit."
     ))
   })
+  observeEvent(input$dismiss, session$reload())
+  
 }
 
 shinyApp(ui, server)
