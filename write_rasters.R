904fbfbb1dcd6bc2dcc6196add3601f645184a9f
blakos1 <blakos1@st.amu.edu.pl>
2022-11-02 00:00:04 +0100

    fixes indexing error

--- write_rasters.R
+++ write_rasters.R
@@ -5,31 +5,20 @@ library(tmap)
 my_sims2_2classes = stack("my_sims2_2classes.tif")
 my_sims2_3classes = stack("my_sims2_3classes.tif")
 
-# change names ------------------------------------------------------------
+# change names of raster layers  ------------------------------------------
 colnums = rep(seq(1:6), 6)
 rownums = rep(1:6, each = 6)
-names(my_sims2_2classes) = paste0("img_2classes_", "col", colnums, "_row", rownums)
+names(my_sims2_2classes) = paste0("img_2classes_", "row", rownums, "_col", colnums)
 
 colnums = rep(seq(1:9), 6)
 rownums = rep(1:6, each = 9)
-names(my_sims2_3classes) = paste0("img_3classes_", "col", colnums, "_row", rownums)
+names(my_sims2_3classes) = paste0("img_3classes_", "row", rownums, "_col", colnums)
 
 # write plots -------------------------------------------------------------
-# export_plots = function(x){
-#   img = tm_shape(x) +
-#     tm_raster(legend.show = FALSE)
-#   
-#   tmap_save(tm = img, filename = paste0("resources/", names(x), ".png"),
-#             width = 600, height = 600)
-# }
-# 
-# raster::stackApply(my_sims2_2classes, indices = c(1,2,3,4),
-#                    fun = export_plots)
-
 merged = stack(my_sims2_2classes, my_sims2_3classes)
 
 for (i in 1:nlayers(merged)){
-  x = my_sims2_2classes[[i]]
+  x = merged[[i]]
   img = tm_shape(x) +
     tm_raster(legend.show = FALSE)
   tmap_save(tm = img, filename = paste0("resources/", names(x), ".png"),
