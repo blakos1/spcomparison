ed5d9105ad0610a38b6c2a7563e9dcd5711b2ebf
blakos1 <blakos1@st.amu.edu.pl>
2022-11-02 00:01:39 +0100

    image sampling test

--- test.R
+++ test.R
@@ -1,10 +1,64 @@
-simulate_composition = function(fract_dim){
-  sim1 = nlm_fbm(100, 100, fract_dim = fract_dim)
-  sim1
+library(stringr)
+# library(NLMR)
+# simulate_composition = function(fract_dim){
+#   sim1 = nlm_fbm(100, 100, fract_dim = fract_dim)
+#   sim1
+# }
+# 
+# x = simulate_composition(0.2)
+# y = simulate_composition(1.6)
+# 
+# plot(x)
+# plot(y)
+
+filename_df = as.data.frame(list.files(path = "resources", full.names = TRUE))
+colnames(filename_df) = "filenames"
+
+filename_df$classes = as.integer(substr(filename_df$filenames, 15,15))
+filename_df$row = as.integer(substr(filename_df$filenames, 27,27))
+filename_df$col = as.integer(substr(filename_df$filenames, 32,32))
+
+
+#2 klasy:
+#6 par po 1 na każdą kolumnę
+#6 par po 1 na każdy wiersz
+
+#3 klasy:
+#6 par po 1 na każdą kolumnę (powinno być 9 - jak naprawić?)
+#6 par po 1 na każdy wiersz
+
+dataset = list()
+
+for(i in 2:3){ #pick num of classes
+  for(j in 1:2){ #pick row or col
+    for(k in 1:6){ #pick items in rows/cols
+      
+      if (j == 1){
+        subset_df = subset(filename_df, classes == i & col == k)
+      } else {
+        subset_df = subset(filename_df, classes == i & row == k)
+      }
+      
+      smpl = subset_df$filenames %>% 
+        combn(m = 2) %>% 
+        as.data.frame() %>%
+        as.list() %>% 
+        sample(size = 1)
+      
+      dataset = append(dataset, smpl)
+    }
+  }
 }
 
-x = simulate_composition(0.2)
-y = simulate_composition(1.2)
+subset2 = subset(filename_df, classes == 2)$filenames %>% 
+  combn(m = 2) %>% 
+  as.data.frame() %>%
+  as.list()
+subset3 = subset(filename_df, classes == 3)$filenames %>% 
+  combn(m = 2) %>% 
+  as.data.frame() %>%
+  as.list()
 
-plot(x)
-plot(y)
+df = as.list(c(subset2, subset3))
+
+residuals_df = subset(df, !(df %in% dataset)) 
