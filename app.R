eefd8974717201c828281d127a5fab4035bfd986
blakos1 <blakos1@st.amu.edu.pl>
2022-11-13 19:17:31 +0100

    testing image sampling fix 1

--- app.R
+++ app.R
@@ -20,149 +20,133 @@ sheet_test = read_sheet(gsheets, col_names = FALSE)
 # Add directory of static resources to Shiny's web server
 addResourcePath(prefix = "resources", directoryPath = "resources")
 
-# image sampling ----------------------------------------------------------
-filename_df = as.data.frame(list.files(path = "resources", full.names = TRUE))
-colnames(filename_df) = "filenames"
-
-filename_df$classes = as.integer(substr(filename_df$filenames, 15,15))
-filename_df$row = as.integer(substr(filename_df$filenames, 27,27))
-filename_df$col = as.integer(substr(filename_df$filenames, 32,32))
-
-dataset = list()
-samplesize = 12
-
-#2 klasy
-for(j in 1:2){ #pick row or col
-  for(k in 1:6){ #pick items in rows/cols
-    
-    if (j == 2){
-      subset_df = subset(filename_df, classes == 2 & col == k)
-    } else {
-      subset_df = subset(filename_df, classes == 2 & row == k)
-    }
-    
-    smpl = subset_df$filenames %>% 
-      combn(m = 2) %>% 
-      as.data.frame() %>%
-      as.list() %>% 
-      sample(size = 1)
-    
-    dataset = append(dataset, smpl)
-  }
-}
-
-#wybranie wszystkich obrazów z 2 klasami
-subset2 = subset(filename_df, classes == 2)$filenames %>% 
-  combn(m = 2) %>% 
-  as.data.frame() %>%
-  as.list()
-
-#samplowanie tylko z reszty obrazów (2 klasy)
-smpl2 = subset(subset2, !(subset2 %in% dataset)) %>% 
-  sample(size = samplesize)
-
-dataset = append(dataset, smpl2)
-
-#3 klasy
-for(j in 1:2){ #pick row or col
-  for(k in 1:6){ #pick items in rows/cols
-    
-    if (j == 2){
-      subset_df = subset(filename_df, classes == 3 & col == k)
-    } else {
-      subset_df = subset(filename_df, classes == 3 & row == k)
-    }
-    
-    smpl = subset_df$filenames %>% 
-      combn(m = 2) %>% 
-      as.data.frame() %>%
-      as.list() %>% 
-      sample(size = 1)
-    
-    dataset = append(dataset, smpl)
-  }
-}
-
-#wybranie wszystkich obrazów z 3 klasami
-subset3 = subset(filename_df, classes == 3)$filenames %>% 
-  combn(m = 2) %>% 
-  as.data.frame() %>%
-  as.list()
-
-#samplowanie tylko z reszty obrazów (3 klasy)
-smpl3 = subset(subset3, !(subset3 %in% dataset)) %>% 
-  sample(size = samplesize)
-
-dataset = append(dataset, smpl3)
-
-
-# questions ---------------------------------------------------------------
-# dataset = combn(myImgResources, 2)
-# dataset = as.data.frame(dataset)
-# dataset = as.list(dataset)
-# 
-# datasample = sample(dataset, 10)
-# x[[1]][1]
-datasample = dataset
-
-questions_df = tibble::tibble()
-
-for (i in 1:length(datasample)){
-  question_id = datasample[[i]] %>% 
-    stringr::str_remove_all("resources/") %>%
-    stringr::str_remove_all(".png") %>%
-    paste(collapse = '+')
-  
-  question = tibble::tibble(question = list(div(#style="text-align:center",
-                                                img(src=datasample[[i]][1], width="49%", height="49%"),
-                                                img(src=datasample[[i]][2], width="49%", height="49%"),
-                                                br(),
-                                                paste0("Pytanie ", i, ": Określ podobieństwo (ilość kolorów i ich ułożenie) między dwoma obrazami")
-                                                )),
-                            option = NA,
-                            input_type = "textSlider",
-                            input_id = question_id,
-                            dependence = NA,
-                            dependence_value = NA,
-                            required = T,
-                            page = i)
-  
-  
-  questions_df = rbind(questions_df, question)
-}
-
-
 # app ---------------------------------------------------------------------
-sliderScale <<- c(
-  "Brak",
-  "Bardzo male",
-  "Niewielkie ",
-  "Lekkie",
-  "Duze",
-  "Calkowite"
-) #global variable bo inaczej nie dziala w linijce 63
-
-extendInputType(input_type = "textSlider", {
-  shinyWidgets::sliderTextInput(
-    inputId = surveyID(),
-    label = surveyLabel(),
-    force_edges = TRUE,
-    choices = sliderScale,
-    selected = sliderScale[4],
-    grid = TRUE
-  ) 
-})
-
-ui <- fluidPage(
-  surveyOutput(df = questions_df,
-               survey_title = "Zmiany struktury przestrzennej kategorii pokrycia terenu",
-               survey_description = "Celem ankiety jest określenie różnic między
-               postrzeganiem zmian struktur przestrzennych przez człowieka
-               a uzyskanymi wartościami różnych miar niepodobieństwa.",
-               theme = "#F4D03F") #"#F9E79F"?
-)
-
 server <- function(input, output, session) {
+  
+  sliderScale <<- c(
+    "Brak",
+    "Bardzo male",
+    "Niewielkie ",
+    "Lekkie",
+    "Duze",
+    "Calkowite"
+  ) #global variable bo inaczej nie dziala w linijce 63
+  
+  extendInputType(input_type = "textSlider", {
+    shinyWidgets::sliderTextInput(
+      inputId = surveyID(),
+      label = surveyLabel(),
+      force_edges = TRUE,
+      choices = sliderScale,
+      selected = sliderScale[4],
+      grid = TRUE
+    ) 
+  })
+  
+  filename_df = as.data.frame(list.files(path = "resources", full.names = TRUE))
+  colnames(filename_df) = "filenames"
+  
+  filename_df$classes = as.integer(substr(filename_df$filenames, 15,15))
+  filename_df$row = as.integer(substr(filename_df$filenames, 27,27))
+  filename_df$col = as.integer(substr(filename_df$filenames, 32,32))
+  
+  dataset = list()
+  samplesize = 12
+  
+  #2 klasy
+  for(j in 1:2){ #pick row or col
+    for(k in 1:6){ #pick items in rows/cols
+      
+      if (j == 2){
+        subset_df = subset(filename_df, classes == 2 & col == k)
+      } else {
+        subset_df = subset(filename_df, classes == 2 & row == k)
+      }
+      
+      smpl = subset_df$filenames %>% 
+        combn(m = 2) %>% 
+        as.data.frame() %>%
+        as.list() %>% 
+        sample(size = 1)
+      
+      dataset = append(dataset, smpl)
+    }
+  }
+  
+  #wybranie wszystkich obrazów z 2 klasami
+  subset2 = subset(filename_df, classes == 2)$filenames %>% 
+    combn(m = 2) %>% 
+    as.data.frame() %>%
+    as.list()
+  
+  #samplowanie tylko z reszty obrazów (2 klasy)
+  smpl2 = subset(subset2, !(subset2 %in% dataset)) %>% 
+    sample(size = samplesize)
+  
+  dataset = append(dataset, smpl2)
+  
+  #3 klasy
+  for(j in 1:2){ #pick row or col
+    for(k in 1:6){ #pick items in rows/cols
+      
+      if (j == 2){
+        subset_df = subset(filename_df, classes == 3 & col == k)
+      } else {
+        subset_df = subset(filename_df, classes == 3 & row == k)
+      }
+      
+      smpl = subset_df$filenames %>% 
+        combn(m = 2) %>% 
+        as.data.frame() %>%
+        as.list() %>% 
+        sample(size = 1)
+      
+      dataset = append(dataset, smpl)
+    }
+  }
+  
+  #wybranie wszystkich obrazów z 3 klasami
+  subset3 = subset(filename_df, classes == 3)$filenames %>% 
+    combn(m = 2) %>% 
+    as.data.frame() %>%
+    as.list()
+  
+  #samplowanie tylko z reszty obrazów (3 klasy)
+  smpl3 = subset(subset3, !(subset3 %in% dataset)) %>% 
+    sample(size = samplesize)
+  
+  dataset = append(dataset, smpl3)
+  
+  datasample = dataset
+  questions_df = tibble::tibble()
+  
+  for (i in 1:length(datasample)){
+    question_id = datasample[[i]] %>% 
+      stringr::str_remove_all("resources/") %>%
+      stringr::str_remove_all(".png") %>%
+      paste(collapse = '+')
+    
+    question = tibble::tibble(question = list(div(#style="text-align:center",
+      img(src=datasample[[i]][1], width="49%", height="49%"),
+      img(src=datasample[[i]][2], width="49%", height="49%"),
+      br(),
+      paste0("Pytanie ", i, ": Określ podobieństwo (ilość kolorów i ich ułożenie) między dwoma obrazami")
+    )),
+    option = NA,
+    input_type = "textSlider",
+    input_id = question_id,
+    dependence = NA,
+    dependence_value = NA,
+    required = T,
+    page = i)
+    
+    
+    questions_df = rbind(questions_df, question)
+  }
+  
+  
+  
+  
   renderSurvey()
   
   observeEvent(input$submit, {
@@ -181,5 +165,7 @@ server <- function(input, output, session) {
   observeEvent(input$dismiss, session$reload())
   
 }
+
+
 
 shinyApp(ui, server)
