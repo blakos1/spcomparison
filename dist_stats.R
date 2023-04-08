f86eee1fbe9a5c57d66237c14fdbbfa12c928826
blakos1 <blakos1@st.amu.edu.pl>
2023-04-08 18:57:50 +0200

    adds hcluster plots

--- dist_stats.R
+++ dist_stats.R
@@ -3,154 +3,125 @@ library(cluster)    # clustering algorithms
 library(factoextra) # clustering visualization
 library(dendextend) # for comparing two dendrograms
 
+
 # read files --------------------------------------------------------------
 data2 = read.csv("data/2classes_dist.csv")
-# data2 = read.csv("data/3classes_dist.csv")
+data3 = read.csv("data/3classes_dist.csv")
+merged = rbind(data2, data3)
 
 # remove inf's & na's -----------------------------------------------------
 data2[sapply(data2, is.infinite)] = NA
-data2 = subset(data2, select = -c(X, minkowski))
-data2 = na.omit(data2)
+data22 = data2 %>% 
+  subset(select = -c(X, minkowski)) %>% 
+  na.omit()
+
+data3[sapply(data3, is.infinite)] = NA
+data33 = data3 %>% 
+  subset(select = -c(X, minkowski)) %>% 
+  na.omit()
+
+nrow(data2) + nrow(data3) - nrow(data22) - nrow(data33) #aż tyle danych ma jakieś wartości NA lub Inf
 
 # transpose dataframe -----------------------------------------------------
-data2_2 = data2 %>% 
-  pivot_longer(cols = c(1:ncol(data2)-1),
+data22_t = data22 %>% 
+  pivot_longer(cols = c(1:ncol(data22)-1),
                names_to = "method", values_to = "value") %>% 
   pivot_wider(names_from = "questionID", values_from = "value")
 
-names = data2_2$method
-data2_2 = as.data.frame(subset(data2_2, select = -c(method)))
-rownames(data2_2) = names
+names = data22_t$method
+data22_t = as.data.frame(subset(data22_t, select = -c(method)))
+rownames(data22_t) = names
 
-colnames(data2_2) = c(1:ncol(data2_2))
+colnames(data22_t) = c(1:ncol(data22_t))
+
+
+data33_t = data33 %>% 
+  pivot_longer(cols = c(1:ncol(data33)-1),
+               names_to = "method", values_to = "value") %>% 
+  pivot_wider(names_from = "questionID", values_from = "value")
+
+names = data33_t$method
+data33_t = as.data.frame(subset(data33_t, select = -c(method)))
+rownames(data33_t) = names
+
+colnames(data33_t) = c(1:ncol(data33_t))
 
 # scale the values --------------------------------------------------------
-data2_2 = scale(data2_2)
+data22_scaled = scale(data22_t)
+data33_scaled = scale(data33_t)
 
-# HCLUST ------------------------------------------------------------------
-df = data2_2
-
-# Dissimilarity matrix
-d <- dist(df, method = "euclidean")
-
-# Hierarchical clustering using Complete Linkage
-hc1 <- hclust(d, method = "complete")
-
-# Plot the obtained dendrogram
-plot(as.dendrogram(hc1), horiz = TRUE)
-plot(hc1, cex = 0.6, hang = -1)
-
-
-# dendro ------------------------------------------------------------------
-library(ggdendro)
-dendr <- dendro_data(hc1, type="rectangle") #convert cluster object to use with ggplot
-
-ggplot() + 
-  geom_segment(data=segment(dendr), aes(x=x, y=y, xend=xend, yend=yend)) + 
-  geom_text(data=label(dendr), aes(x=x, y= c(y -0.5), label=label, hjust=1), size=3) +
-  coord_flip() +
-  # scale_y_reverse(expand=c(0.2, 0)) +
-  theme(axis.line.y=element_blank(),
-        axis.ticks.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.title.y=element_blank(),
-        panel.background=element_rect(fill="white"),
-        panel.grid=element_blank())
-
-
-
-# agnes -------------------------------------------------------------------
-# Compute with agnes
-hc2 <- agnes(df, method = "complete")
-
-# Agglomerative coefficient
-hc2$ac
-## [1] 0.9204506
-#agglomerative coefficient, which measures the amount of clustering structure found
-#(values closer to 1 suggest strong clustering structure)
-
-
+# AGNES -------------------------------------------------------------------
 # methods to assess
 m <- c( "average", "single", "complete", "ward")
 names(m) <- c( "average", "single", "complete", "ward")
 
-# function to compute coefficient
-ac <- function(x) {
-  agnes(df, method = x)$ac
+# agglomerative coefficient
+ac = data.frame(method = m)
+for (i in 1:length(ac$method)){
+  ac$two[i] = agnes(data22_scaled, method = ac$method[i])$ac
+  ac$three[i] = agnes(data33_scaled, method = ac$method[i])$ac
 }
 
-map_dbl(m, ac)
-##   average    single  complete      ward 
-## 0.9085222 0.9005353 0.9204506 0.9231909 
+# divise coefficient / DIANA
+diana(data22_scaled)$dc
+diana(data33_scaled)$dc
 
-hc3 <- agnes(df, method = "ward")
-pltree(hc3, cex = 0.6, hang = -1, main = "Dendrogram of agnes")
+# Cut agnes() tree into 5 groups
+agnes22 <- agnes(data22_scaled, method = "ward")
+sub_grp22 = cutree(as.hclust(agnes22), k = 5)
 
-
-# diana -------------------------------------------------------------------
-# compute divisive hierarchical clustering
-hc4 <- diana(df)
-
-# Divise coefficient; amount of clustering structure found
-hc4$dc
-## [1] 0.9174377
-
-# plot dendrogram
-pltree(hc4, cex = 0.6, hang = -1, main = "Dendrogram of diana")
-
-# Ward's method
-hc5 <- hclust(d, method = "ward.D2" )
-
-# Cut tree into 4 groups
-sub_grp <- cutree(hc5, k = 8)
+agnes33 <- agnes(data33_scaled, method = "ward")
+sub_grp33 = cutree(as.hclust(agnes33), k = 5)
 
 # Number of members in each cluster
-table(sub_grp)
+table(sub_grp22)
+table(sub_grp33)
 
-df = df %>%
+# add groups to df
+data22_mut = data22_scaled %>%
   as.data.frame() %>% 
-  mutate(cluster = sub_grp)
+  mutate(cluster = sub_grp22)
+
+data33_mut = data33_scaled %>%
+  as.data.frame() %>% 
+  mutate(cluster = sub_grp33)
+
+# agnes plots
+pltree(agnes22, cex = 0.6, hang = -1, main = "Dendrogram of agnes") 
+rect.hclust(agnes22, k = 5, border = 2:5)
+
+fviz_cluster(list(data = data22_mut, cluster = sub_grp22))
+
+pltree(agnes33, cex = 0.6, hang = -1, main = "Dendrogram of agnes") 
+rect.hclust(agnes33, k = 5, border = 2:5)
+
+fviz_cluster(list(data = data33_mut, cluster = sub_grp33))
 
 
-plot(hc5, cex = 0.6)
-rect.hclust(hc5, k = 8, border = 2:5)
 
-fviz_cluster(list(data = df, cluster = sub_grp))
-
-# Cut agnes() tree into 4 groups
-hc_a <- agnes(df, method = "ward")
-cutree(as.hclust(hc_a), k = 8)
-
-# Cut diana() tree into 4 groups
-hc_d <- diana(df)
-cutree(as.hclust(hc_d), k = 8)
-
+# entanglement ------------------------------------------------------------
 # Compute distance matrix
-res.dist <- dist(df, method = "euclidean")
+res.dist22 <- dist(data22_scaled, method = "euclidean")
+res.dist33 <- dist(data33_scaled, method = "euclidean")
 
 # Compute 2 hierarchical clusterings
-hc1 <- hclust(res.dist, method = "complete")
-hc2 <- hclust(res.dist, method = "ward.D2")
+hc22 <- hclust(res.dist22, method = "complete")
+hc33 <- hclust(res.dist33, method = "ward.D2")
 
 # Create two dendrograms
-dend1 <- as.dendrogram (hc1)
-dend2 <- as.dendrogram (hc2)
+dend22 <- as.dendrogram(hc22)
+dend33 <- as.dendrogram(hc33)
 
-dend_list <- dendlist(dend1, dend2)
+dend_list <- dendlist(dend22, dend33)
 
-tanglegram(dend1, dend2,
+round(entanglement(dend_list), 2) #ENTANGLEMENT
+
+tanglegram(dend22, dend33,
            highlight_distinct_edges = FALSE, # Turn-off dashed lines
            common_subtrees_color_lines = FALSE, # Turn-off line colors
            common_subtrees_color_branches = TRUE, # Color common branches 
            main = paste("entanglement =", round(entanglement(dend_list), 2))
 )
+
 # Entanglement is a measure between 1 (full entanglement) and 0 (no entanglement).
 # A lower entanglement coefficient corresponds to a good alignment. 
-
-
-# Determining Optimal Clusters --------------------------------------------
-fviz_nbclust(df, FUN = hcut, method = "wss")
-
-fviz_nbclust(df, FUN = hcut, method = "silhouette")
-
-
